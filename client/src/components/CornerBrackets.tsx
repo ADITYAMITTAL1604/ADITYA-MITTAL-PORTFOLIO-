5c12ffe122dc37931d2074867865b15390e82bb5
Aditya Mittal <adityamittal568@gmail.com>
2026-07-31 20:14:43 +0530

    style: remove outer border line from corner brackets

--- client/src/components/CornerBrackets.tsx
+++ client/src/components/CornerBrackets.tsx
@@ -4,7 +4,7 @@
  */
 export default function CornerBrackets() {
   return (
-    <div className="pointer-events-none fixed top-4 left-1/2 -translate-x-1/2 z-[50] w-[calc(100%-32px)] h-[calc(100vh-32px)] border border-white/10 mx-auto">
+    <div className="pointer-events-none fixed top-4 left-1/2 -translate-x-1/2 z-[50] w-[calc(100%-32px)] h-[calc(100vh-32px)] mx-auto">
       {/* Top-left corner */}
       <div className="absolute top-0 left-0 w-4 h-[2px] bg-white/30" />
       <div className="absolute top-0 left-0 w-[2px] h-4 bg-white/30" />
